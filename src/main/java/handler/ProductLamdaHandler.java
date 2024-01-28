7ae21fbcac0c50a52ba2e77706c5220598a6c726
matheus.carneiro <matheus.carneiro@darede.com.br>
2024-01-28 17:08:30 -0300

    Criação método delete

--- src/main/java/handler/ProductLamdaHandler.java
+++ src/main/java/handler/ProductLamdaHandler.java
@@ -108,6 +108,41 @@ public class ProductLamdaHandler implements RequestStreamHandler {
         writer.write(responseObject.toString());
         reader.close();
         writer.close();
+    }
+    @SuppressWarnings("unchecked")
+    public void handleDeleteRequest(InputStream input, OutputStream output, Context context) throws IOException {
 
+        OutputStreamWriter writer = new OutputStreamWriter(output);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+        JSONParser parser = new JSONParser(); // this will help us parse the request object
+        JSONObject responseObject = new JSONObject(); // we will add to this object for our api response
+        JSONObject responseBody = new JSONObject();// we will add the item to this object
+
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.defaultClient();
+        DynamoDB dynamoDB = new DynamoDB(client);
+
+        try {
+            JSONObject reqObject = (JSONObject) parser.parse(reader);
+
+            if(reqObject.get("pathParameters")!=null) {
+                JSONObject pps = (JSONObject) reqObject.get("pathParameters");
+
+                if(pps.get("id")!=null) {
+                    int id = Integer.parseInt((String) pps.get("id"));
+                    dynamoDB.getTable(DYNAMO_TABLE).deleteItem("id", id);
+                }
+            }
+            responseBody.put("message", "Item Deleted");
+            responseObject.put("statusCode", 200);
+            responseObject.put("body", responseBody.toString());
+
+        } catch (Exception e) {
+            responseObject.put("statusCode", 400);
+            responseObject.put("error", e);
+        }
+
+        writer.write(responseObject.toString());
+        reader.close();
+        writer.close();
     }
 }
